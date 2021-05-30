5c4a14d402c180598651d270bd0b5ecb4a84b420
Raghib Aftab <raghib@raghibs-air.lan>
2021-05-30 01:21:53 -0500

    added simulation logic

--- api/sim.py
+++ api/sim.py
@@ -50,12 +50,12 @@ class Simulation:
                 time.sleep(1)
             
         if ballFirst is 1:
-            print(self.player1.name + ' gets ball first')
-            self.outcomeQueue.append(self.player1.name + ' gets ball first')
+            print(self.player1.name + ' gets ball first\n\n')
+            self.outcomeQueue.append(self.player1.name + ' gets ball first\n\n')
             self.player1.ball = True
         else:
-            print(self.player2.name + ' gets ball first')
-            self.outcomeQueue.append(self.player2.name + ' gets ball first')
+            print(self.player2.name + ' gets ball first\n\n')
+            self.outcomeQueue.append(self.player2.name + ' gets ball first\n\n')
             self.player2.ball = True
 
         if self.player1.ball != True:
@@ -63,36 +63,115 @@ class Simulation:
         
     #! Simulate game
     def sim(self):
+        # Track score
         p1Score = 0
         p2Score = 0
+
+        # Flag to determine if possesion is changed
+        possesion = True
+
+        # Players odds of attempting a three
         player1threeAttempt = self.player1.threePointsMade/self.player1.fieldGoalsTaken
         player2threeAttempt = self.player2.threePointsMade/self.player2.fieldGoalsTaken
+
+        # Players odds of making a midrange shot #! Need to recalculate using defensive rating
+        p1FieldGoal = self.player1.fieldGoalPercentage * 1.3
+        p2FieldGoal = self.player2.fieldGoalPercentage * 1.3
+
+        # Players odds of making a midrange shot #! Need to recalculate using defensive rating        
+        p1Three = self.player1.threePointPercentage * 1.3
+        p2Three = self.player2.threePointPercentage * 1.3
         
+        ct = 1
         #! Simulate Game
         while p1Score <= self.scoreLimit or p2Score <= self.scoreLimit:
-            time.sleep(1)
+            print(ct)
+            time.sleep(2) # delay sim results
+
+            # PLAYER 1 SIMULATION
             if self.player1.ball == True:
+
+                # odds for taking a midrange shot or 2pt
                 c = random.randrange(101)
                 if c < player1threeAttempt * 100:
-                    print(self.player1.name + ' shot a 2-pointer')
-                    p1Score += 2
-                else:
-                    print(self.player1.name + ' shot a 1-pointer')
-                    p1Score += 1
 
+                    # 2 point shot attempted
+                    print(self.player1.name + ' attempted a a 2-pointer')
+
+                    # Shot made
+                    if random.randrange(101) < p1Three * 100:
+                        # Add points and keep possesion
+                        print(self.player1.name + ' made a 2-pointer')
+                        p1Score += 2
+                        possesion = False
+                    else: # shot missed
+                        print(self.player1.name + ' bricked the shot')
+                        #! use rebound logic used to get possesion
+
+                        # change possesion - add condition when rebounding is implemented
+                        possesion = True
+                else:
+
+                    # 1 point shot attempted
+                    print(self.player1.name + ' attempted a 1-point shot')
+
+                    # shot made
+                    if random.randrange(101) < p1FieldGoal * 100:
+                        print(self.player1.name + ' made a 1-point shot')
+                        p1Score += 1
+                        possesion = False
+                    else: # shot missed
+                        print(self.player1.name + ' bricked the shot')
+                        #! use rebound logic used to get possesion
+
+                        # change possesion - add condition when rebounding is implemented
+                        possesion = True
+
+            # PLAYER 2 SIMULATION
             if self.player2.ball == True:
+
+                # odds for taking a midrange shot or 2pt
                 c = random.randrange(101)
                 if c < player2threeAttempt * 100:
-                    print(self.player2.name + ' shot a 2-pointer')
-                    p2Score += 2
-                else:
-                    print(self.player2.name + ' shot a 1-pointer')
-                    p2Score += 1
+                    # 2 point shot attempted
+                    print(self.player2.name + ' attempted a a 2-pointer')
 
+                    # Shot made
+                    if random.randrange(101) < p2Three * 100:
+                        # Add points and keep possesion
+                        print(self.player2.name + ' made a 2-pointer')
+                        p2Score += 2
+                        possesion = False
+                    else: # shot missed
+                        print(self.player2.name + ' bricked the shot')
+                        #! use rebound logic used to get possesion
+
+                        # change possesion - add condition when rebounding is implemented
+                        possesion = True
+                else:
+                    # 1 point shot attempted
+                    print(self.player2.name + ' attempted a 1-point shot')
+
+                    # shot made
+                    if random.randrange(101) < p2FieldGoal * 100:
+                        print(self.player2.name + ' made a 1-point shot')
+                        p2Score += 1
+                        possesion = False
+                    else: # shot missed
+                        print(self.player2.name + ' bricked the shot')
+                        #! use rebound logic used to get possesion
+
+                        # change possesion - add condition when rebounding is implemented
+                        possesion = True
+
+            print('SCORE:')
             print(f'{self.player1.name}: {p1Score}')
             print(f'{self.player2.name}: {p2Score}')
             print()
-            self.player1.ball, self.player2.ball = self.player2.ball, self.player1.ball
+            if possesion == True:
+                self.player1.ball, self.player2.ball = self.player2.ball, self.player1.ball
+
+            ct += 1 # inc possesion counter
 
             if p1Score >= 11 or p2Score >= 11:
                 break

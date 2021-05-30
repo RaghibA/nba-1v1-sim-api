727237ebe53409afc7271bacbe51c8bbf0a0b361
Raghib Aftab <raghib@raghibs-air.lan>
2021-05-30 01:27:43 -0500

    benching curry for now

--- api/app.py
+++ api/app.py
@@ -6,9 +6,9 @@ from player import player
 def __main__():
     lbj = player('Lebron', .5, .34, 19.6, 4.4, 7.4, .8, 1.6)
     kobe = player('Kobe', .447, .329, 19.5, 4.1, 5.2, .5, 1.4)
-    steph = player('steph', .477, .433, 17.4, 8.6, 4.6, .2, 1.7)
+    steph = player('Steph', .477, .433, 17.4, 8.6, 4.6, .2, 1.7)
 
-    sim = Simulation(lbj,steph,11)
+    sim = Simulation(lbj,kobe,11)
     sim.play()
 
 if __name__ == '__main__':

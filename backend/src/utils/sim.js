b30ca244072a16e78ffa38a3450da98a0f8a605d
Raghib Aftab <raghib@raghibs-air.lan>
2021-06-22 16:13:13 -0500

    Began development on simulation utility

--- backend/src/utils/sim.js
+++ backend/src/utils/sim.js
@@ -1,10 +1,10 @@
 // TODO: Migrate simulation logic from python to node.js
 
-const shootForBall = () => { }
-
-const simGame = () => { }
-
-module.exports = {
-  shootForBall,
-  simGame
+const simGame = (player1, player2, callback) => {
+  // TEST CODE:
+  callback(undefined, { test: 'success', player1, player2 })
+  
+  // TODO: Simulate game and send simulation JSON in callback result
 }
+
+module.exports = simGame

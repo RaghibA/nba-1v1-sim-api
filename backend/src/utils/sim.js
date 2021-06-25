ffcbbdba726d7c4e93cb2b251ea67bf2c239c783
Raghib Aftab <raghib@raghibs-air.lan>
2021-06-25 18:50:43 -0500

    Simulation updated

--- backend/src/utils/sim.js
+++ backend/src/utils/sim.js
@@ -1,10 +1,61 @@
 // TODO: Migrate simulation logic from python to node.js
 
 const simGame = (player1, player2, callback) => {
-  // TEST CODE:
-  callback(undefined, { test: 'success', player1, player2 })
+
+  //! TEST CODE: Send dummy sim results
+  const simResults = {
+    limit: 4,
+    player1,
+    player2,
+    turns: [
+      {
+        turn: 1,
+        p1Score: 0,
+        p2Score: 0,
+        pos: 1
+      },
+      {
+        turn: 2,
+        p1Score: 0,
+        p2Score: 1,
+        pos: 1
+      },
+      {
+        turn: 3,
+        p1Score: 0,
+        p2Score: 1,
+        pos: 2
+      },
+      {
+        turn: 4,
+        p1Score: 2,
+        p2Score: 1,
+        pos: 2
+      },
+      {
+        turn: 5,
+        p1Score: 2,
+        p2Score: 1,
+        pos: 1
+      },
+      {
+        turn: 6,
+        p1Score: 2,
+        p2Score: 3,
+        pos: 1
+      },
+      {
+        turn: 7,
+        p1Score: 2,
+        p2Score: 4,
+        pos: 1
+      },
+    ]
+  }
   
-  // TODO: Simulate game and send simulation JSON in callback result
+  callback(undefined, simResults)
+  
+  // TODO: Implement simulation logic
 }
 
 module.exports = simGame

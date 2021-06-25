90939f9a0a2b28536fbaff0697e19d59282b5e01
Raghib Aftab <raghib@raghibs-air.lan>
2021-06-25 18:41:44 -0500

    Sim endpoint connection working

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,22 +1,22 @@
 const express = require('express')
-const cors = require('cors')
-
-const chalk = require('chalk')
+require('./db/mongoose')
+// const cors = require('cors')
 
 // Import Router
 const simRouter = require('./routers/sim-router')
 
+const chalk = require('chalk')
+
 // INIT Express app
 const app = express()
-app.use(cors)
-
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4004
 const apiURL = 'localhost'
-
 app.use(express.json())
+// app.use(cors)
 app.use(simRouter)
 
 // Listen for connection
-app.listen(port, apiURL, () => {
-  console.log(chalk.blue.inverse('API running at address: ' + apiURL + ':' + port))
-})
+app.listen(port, apiURL, function(err) {
+  if (err) return console.log(err);
+  console.log(chalk.inverse.blue("Listening at http://localhost:" + port));
+});
--- backend/src/routers/sim-router.js
+++ backend/src/routers/sim-router.js
@@ -1,28 +1,28 @@
 const express = require('express')
-
 const Player = require('../models/player-model')
 const simGame = require('../utils/sim')
-
 const router = express.Router()
 
 // API endpoint for simulation
 // Get simulation results
-router.get('/sim/:p1/:p2', async (req, res) => {
-  // store the id of of the player from the req
-  // and use it to retreive the models from db
-  playerOneId = req.params.p1
-  playerTwoId = req.params.p2
+router.get('/sim', async (req, res) => {
+  res.send({msg: "wow"})
 
-  // simulate game, then send results in response body
-  simGame(playerOneId, playerTwoId, (err, result) => {
-    if (err) {
-      // Catch error
-      res.status(500).send(err)
-    } else {
-      // send sim result
-      res.status(200).send(result)
-    }
-  })
+  // // store the id of of the player from the req
+  // // and use it to retreive the models from db
+  // playerOneId = req.params.p1
+  // playerTwoId = req.params.p2
+
+  // // simulate game, then send results in response body
+  // simGame(playerOneId, playerTwoId, (err, result) => {
+  //   if (err) {
+  //     // Catch error
+  //     res.status(500).send(err)
+  //   } else {
+  //     // send sim result
+  //     res.status(200).send(result)
+  //   }
+  // })
 })
 
 module.exports = router

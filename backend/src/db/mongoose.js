48e85e91110efa86b74e76b486f08aea44691130
Raghib Aftab <raghib@raghibs-air.lan>
2021-06-26 15:55:50 -0500

    Mongodb cluster connected

--- backend/src/db/mongoose.js
+++ backend/src/db/mongoose.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
 const chalk = require('chalk')
 
-// TODO: Create mongo cluster with player data
-// mongoose.connect('mongodb://127.0.0.1:27017/nba1v1', {
-//     useNewUrlParser: true,
-//     useCreateIndex: true
-// }, (e) => {
-//     if (e) {
-//         console.log(chalk.red.inverse('ERROR: Unable to connected to MongoDB'))
-//     } else {
-//         console.log(chalk.blue.inverse('Connected to MongoDB'))
-//     }
-// })
+// TODO: Populate mongo cluster with player data
+mongoose.connect('mongodb+srv://user:pass1234@bball1v1.i5ahj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useCreateIndex: true
+}, (e) => {
+    if (e) {
+        console.log(chalk.red.inverse('ERROR: Unable to connected to MongoDB'))
+    } else {
+        console.log(chalk.blue.inverse('Connected to MongoDB'))
+    }
+})

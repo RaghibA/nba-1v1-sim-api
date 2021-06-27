c57160e15288839aa0e9730b645e4ffeaadf973d
Raghib Aftab <raghib@raghibs-air.lan>
2021-06-27 18:18:57 -0500

    Added rebounding and increased shooting percentages

--- backend/src/utils/sim.js
+++ backend/src/utils/sim.js
@@ -31,6 +31,7 @@ const simGame = (playerOneId, playerTwoId, callback) => {
   while (p1Score <= 11 || p2Score <= 11) {
     let swapPos = false
     let shot = 0
+    let madeShot = false
 
     console.log(chalk.greenBright(p1Score + ' ' + p2Score))
     if (pos === 0) { // Player 1 Possesion
@@ -41,15 +42,31 @@ const simGame = (playerOneId, playerTwoId, callback) => {
         let mr = simPercentages.midRangePct(player1.field_goal_pct, player2.def_rating)
         shot = 1
 
-        if (mr === true) { p1Score += 1 }
-        else { swapPos = true } // TODO: Add rebound logic
+        if (mr === true) {
+          p1Score += 1
+          madeShot = true
+        }
+        else { // Attempt offensive rebound
+          let rb = simPercentages.offensiveRebound(player1.rebounds, player1.height,
+            player2.rebounds, player2.height)
+          if (rb === true) { swapPos = false }
+          else{ swapPos = true}
+        } 
         
       } else { // Take 2 point shot
         let tp = simPercentages.threePct(player1.three_pt_pct, player2.def_rating)
         shot = 2
 
-        if (tp === true) { p1Score += 2 }
-        else { swapPos = true } // TODO: Add rebound logic
+        if (tp === true) {
+          p1Score += 2
+          madeShot = true
+        }
+        else { // Attempt offensive rebound
+          let rb = simPercentages.offensiveRebound(player1.rebounds, player1.height,
+            player2.rebounds, player2.height)
+          if (rb === true) { swapPos = false }
+          else{ swapPos = true}
+        } 
       }
 
     } // End Player 1
@@ -60,20 +77,36 @@ const simGame = (playerOneId, playerTwoId, callback) => {
         let mr = simPercentages.midRangePct(player2.field_goal_pct, player1.def_rating)
         shot = 1
 
-        if (mr === true) { p2Score += 1 }
-        else { swapPos = true } // TODO: Add rebound logic
+        if (mr === true) {
+          p2Score += 1
+          madeShot = true
+        }
+        else { // Attempt offensive rebound
+          let rb = simPercentages.offensiveRebound(player2.rebounds, player2.height,
+            player1.rebounds, player1.height)
+          if (rb === true) { swapPos = false }
+          else{ swapPos = true}
+        } 
         
       } else { // Take 2 point shot
         let tp = simPercentages.threePct(player2.three_pt_pct, player1.def_rating)
         shot = 2
 
-        if (tp === true) { p2Score += 2 }
-        else { swapPos = true } // TODO: Add rebound logic
+        if (tp === true) {
+          p2Score += 2
+          madeShot = true
+        }
+        else { // Attempt offensive rebound
+          let rb = simPercentages.offensiveRebound(player2.rebounds, player2.height,
+            player1.rebounds, player1.height)
+          if (rb === true) { swapPos = false }
+          else{ swapPos = true}
+        } 
       } // End Player 2 
     }
 
     //! Update simulation data
-    sim.turns.push({ turn, p1Score, p2Score, pos, shot })
+    sim.turns.push({ turn, p1Score, p2Score, pos, shot, madeShot })
           
     if (swapPos === true) { // Change possesion 
       if (pos === 1) { pos = 0 }
--- backend/src/utils/simPercentages.js
+++ backend/src/utils/simPercentages.js
@@ -3,25 +3,47 @@
 // Odds of a player taking a three-point shot
 function takeThree(threesMade, fgTaken) {
   const pct = threesMade/ fgTaken
-  return Math.floor(Math.random() * 100) <= pct
+  return Math.floor(Math.random() * 100) <= pct * 100
 }
 
 // Odds of player making a 1 point field goal
 function midRangePct(p1_fg_pct, p2_def_rating) {
   const leagueAvgOffRating = 112
   const adjPercentage = (p2_def_rating / leagueAvgOffRating) * p1_fg_pct
-  return Math.floor(Math.random() * 100) <= adjPercentage
+  return Math.floor(Math.random() * 100) <= adjPercentage * 1.13
 }
 
 // Odds of player making a 2 point field goal
 function threePct(p1_tp_pct, p2_def_rating) {
   const leagueAvgOffRating = 112
   const adjPercentage = (p2_def_rating / leagueAvgOffRating) * p1_tp_pct
-  return Math.floor(Math.random() * 100) <= adjPercentage
+  return Math.floor(Math.random() * 100) <= adjPercentage * 1.13
+}
+
+function offensiveRebound(p1_rebound, p1_height, p2_rebound, p2_height) {
+  const h_diff = p1_height - p2_height
+  const t_rb = p1_rebound + p2_rebound
+  let rb = 0
+
+  // Offensive robound odds: P1 RPG / Total RPG of both players, scaled with height difference
+  if (h_diff < 5 || h_diff > -5) { // minimal height difference
+    rb = p1_rebound / t_rb
+  } else if (-10 < h_diff < -5) { // Offensive player is noticably shorter
+    rb = (p1_rebound / t_rb) * .85
+  } else if (h_diff < -10) { // Offensive player is significantly shorter
+    rb = (p1_rebound / t_rb) * .70
+  } else if (10 > h_diff > 5) { // Offensive player is noticably taller
+    rb = (p1_rebound / t_rb) * 1.15
+  } else if (h_diff > 10) { // Offensive player is significantly taller
+    rb = (p1_rebound / t_rb) * 1.35
+  }
+  
+  return (Math.random() * 100) <= (rb * 100)
 }
 
 module.exports = {
   takeThree,
   midRangePct,
-  threePct
+  threePct,
+  offensiveRebound
 }

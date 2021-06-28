9a546b96cf15380fce1963c41c0fb25ec364cd91
sulemanbutta <sulemanbutta98@gmail.com>
2021-06-28 03:03:28 -0500

    updated frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,38 @@
-import React from "react";
-import './App.css';
-
+//import React from "react";
+import React, {useState, useEffect} from "react";
 function App() {
-  const [data, setData] = React.useState(null);
+  /*
+ const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
-    fetch("http://localhost:4004/sim/lebron/kobe")
+    fetch("http://localhost:4004/sim/1/2/11")
       .then((res) => res.json())
-      .then((data) => console.log(data));
-      //.then((data) => setData(data.message));
+      //.then((data) => console.log(data));
+      .then((data) => setData(data.player1.name));
     }, []);
 
   return (
     <div>
-      <h1>Test</h1>
+          <p>{data}</p>
    </div>
   );
-}
+ */
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:4004/sim/1/2/11")
+      .then(res => {
+        if(res.ok) {
+          return res.json()
+        }
+      }).then(jsonRes => setData(jsonRes.turns))
+  })
 
-export default App;
+
+return (
+  <div>
+        {data.map(data => <div><li>{data.p1Score}</li>
+           <li>{data.p2Score}</li><br></br></div>)}
+ </div>
+);}
+
+ export default App;

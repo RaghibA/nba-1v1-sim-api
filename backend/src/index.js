6e7316d132c1ac8ffbc6a5ad2e425012698ef7fd
Raghib Aftab <raghib@raghibs-air.lan>
2021-06-28 22:10:52 -0500

    Updated for deployment to heroku

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,18 +16,12 @@ const chalk = require('chalk')
 // INIT Express app
 //const app = express()
 const port = process.env.PORT || 4004
-const apiURL = 'localhost'
 app.use(express.json())
 //app.use(cors)
 app.use(simRouter)
-/*
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
-*/
-// Listen for connection
 
-app.listen(port, apiURL, function(err) {
+
+app.listen(port, function(err) {
   if (err) return console.log(err);
-  console.log(chalk.inverse.blue("Listening at http://localhost:" + port));
+  console.log(chalk.inverse.blue("Listening at port " + port));
 });
